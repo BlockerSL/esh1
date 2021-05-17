b6ae0b00e4906219178ac77d3a1d0dfaab4955bf
BlockerSL <82181981+BlockerSL@users.noreply.github.com>
2021-05-17 15:14:13 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,10 @@
 function sahand()
 {
   var Tag = document.getElementById("Tag").value;
-    var bag = document.getElementById("bag").value;
+  var bag = document.getElementById("bag").value;
 
-  if (Tag == "H") {
-    window.open("https://awesome-hamilton-876062.netlify.app/")
+  if (Tag+bag == "H")
+  {
+    window.open("https://awesome-hamilton-876062.netlify.app/");
+
   }
-
-  if (bag == "S"){
-
-    window.open("https://awesome-hamilton-876062.netlify.app/")
-}
-}
