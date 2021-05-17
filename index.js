aa3f6182363e9cf422db6cf0e3e73a2b12dda0de
BlockerSL <82181981+BlockerSL@users.noreply.github.com>
2021-05-17 15:18:36 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function sahand()
   var Tag = document.getElementById("Tag").value;
   var bag = document.getElementById("bag").value;
 
-  if (Tag+bag == "H")
+  if (Tag == "H" + bag == "S")
   {
     window.open("https://awesome-hamilton-876062.netlify.app/");
 
